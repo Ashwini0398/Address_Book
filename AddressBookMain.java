417e7444a037d20ce14dea519a60d0db797f7cdc
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-02-03 20:29:41 +0530

    ADDED: get the details from console

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,30 +1,48 @@
 package com.book;
 
+import java.util.*;
+
 public class AddressBookMain {
 	
-	String firstName;
-    String lastName;
-    String address;
-    String city;
-    String state;
-    int zipCode;
-    String phoneNumber;
+	static Person getDataFromConsole() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Details of Person");
+		System.out.println("Enter the first name");
+		String firstName = sc.next();
+		System.out.println("Enter the Last name");
+		String lastName = sc.next();
+		System.out.println("Enter the Address");
+		String address = sc.next();
+		System.out.println("Enter the City");
+		String city = sc.next();
+		System.out.println("Enter the State");
+		String state = sc.next();
+		System.out.println("Enter the ZipCode");
+		int zipCode = sc.nextInt();
+		System.out.println("Enter the contact number...");
+		String phoneNumber = sc.next();
 
-   AddressBookMain(String firstName, String lastName, String address, String city, String state, int zipCode,String phoneNumber)
-   {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.address = address;
-      this.city = city;
-      this.state = state;
-      this.zipCode = zipCode;
-      this.phoneNumber = phoneNumber;
-   }
+		return new Person(firstName, lastName, address, city, state, zipCode, phoneNumber);
+	}
+	
 
-public static void main(String[] args)
-		{
-			AddressBookMain a = new AddressBookMain("Ashwini","khonde","Nardana","Dhule","Maharastra",415605,"9503076240");
-		}
+	public static void main(String[] args) {
 	
-	
+			Person person = getDataFromConsole(); //get the data from the console
+			int choice;
+			 while(true)
+			 {
+				Scanner sc = new Scanner(System.in);
+				System.out.println("Enter ur choice press 1 For Add person  ");
+				choice = sc.nextInt();
+				switch (choice) {
+				case 1:
+					person = getDataFromConsole();
+					break;
+				default:
+					System.out.println("Enter valid number");
+				}
+			}
+			  
+	}
 }
